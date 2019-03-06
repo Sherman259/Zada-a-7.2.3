39958f0d78af96ece91743badf113cd07443e026
Tihomir Jovic <jtihomir@gmail.com>
2019-03-06 13:08:22 +0100

    Add project files.

---
+++ Zadaća 7.2.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaća_7._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
