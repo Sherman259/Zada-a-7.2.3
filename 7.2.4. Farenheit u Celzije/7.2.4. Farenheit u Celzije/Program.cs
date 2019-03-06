c75d0a2a77301489681dc46af3a5d3805ff75f4a
Tihomir Jovic <jtihomir@gmail.com>
2019-03-06 14:56:41 +0100

    radi

---
+++ 7.2.4. Farenheit u Celzije/7.2.4. Farenheit u Celzije/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._4.Farenheit_u_Celzije
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi stupnjeve Farenheit");
+            double Farenheit = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} F = {1} C", Farenheit FtoC(Farenheit));
+            Console.ReadKey();
+        }
+        static double FtoC(double Farenheit);
+        {
+          return(double) 5/9* (Farenheit-32);
+        }
+        
+
+    }
+}
--- Zadaća 7.2.3/Program.cs
+++ Zadaća 7.2.3/Program.cs
@@ -10,6 +10,32 @@ namespace Zadaća_7._2._3
     {
         static void Main(string[] args)
         {
+            Console.Write("Unesite snagu u KW (kilowatima):");
+            double kw = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Snaga izražena u  konjskim snagama je {0}.", KWtoKS(kw));
+            Console.ReadKey();
+
         }
-    }
-}
+
+
+            
+                static double KWtoKS(double kw)
+                {
+                 return kw * 1.35;
+
+
+                }
+
+
+
+     }
+
+
+ }
+
+
+
+
+    
+
